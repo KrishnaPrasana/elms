ed43c9e62f1fade107f6d0a208b785d6e70a4ffd
vijaykmr45 <vijaykumar342006@gmail.com>
2026-02-01 17:40:11 +0530

    change password api added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -97,3 +97,50 @@ export const login = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
+export const changePassword = async (req,res) => {
+    try {
+
+        const userId = req.user.id; // from JWT middleware
+
+        const { currentPassword, newPassword } = req.body;
+ 
+        if (!currentPassword || !newPassword) {
+
+            return res.status(400).json({ message: "Both passwords are required" });
+
+        }
+ 
+        if (newPassword.length < 6) {
+
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
+
+        }
+ 
+        const user = await User.findByPk(userId);
+
+        if (!user) return res.status(404).json({ message: "User not found" });
+ 
+        const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+        if (!isMatch) {
+
+            return res.status(400).json({ message: "Current password is incorrect" });
+
+        }
+ 
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+ 
+        await user.update({ password: hashedPassword });
+ 
+        res.json({ message: "Password changed successfully" });
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({ message: "Server error" });
+
+    }
+ 
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, changePassword } from "../controllers/authController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 import { authorizeRoles } from "../middlewares/authorizeRoles.js";
 
@@ -11,6 +11,6 @@ router.post("/login", login);
 
 // Register a user (Admin can create employees)
 router.post("/register", protect, authorizeRoles("admin"), register);
-
+router.put("/change-password", protect,changePassword);
 
 export default router;
